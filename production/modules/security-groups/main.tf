e41c9bbc783a52f7ed949b546a967ddbf1192ae9
Alexandru Coarna <coarna.alexandru@gmail.com>
2023-11-19 09:06:53 +0200

    more testing

--- production/modules/security-groups/main.tf
+++ production/modules/security-groups/main.tf
@@ -6,18 +6,26 @@ resource "aws_security_group" "bookingdbinstance_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [aws_vpc.main.cidr_block]
   }
-}
-
-resource "aws_security_group" "ec2_to_bookingdbinstance_sg" {
-  name        = "ec2_to_bookingdbinstance_sg"
-  description = "Allow outbound PostgreSQL traffic"
 
   egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Adjust this based on your security requirements
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
+
+# resource "aws_security_group" "ec2_to_bookingdbinstance_sg" {
+#   name        = "ec2_to_bookingdbinstance_sg"
+#   description = "Allow outbound PostgreSQL traffic"
+
+#   egress {
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     cidr_blocks = [aws_vpc.main.cidr_block]  # Adjust this based on your security requirements
+#   }
+# }
