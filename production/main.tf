e34fd85ca60acf07ec54bc2790bf620928476868
Alexandru Coarna <coarna.alexandru@gmail.com>
2023-11-19 11:39:41 +0200

    clean code

--- production/main.tf
+++ production/main.tf
@@ -92,12 +92,6 @@ resource "aws_elastic_beanstalk_environment" "env" {
     value     = "t3.micro"
   }
 
-  # setting {
-  #   namespace = "aws:autoscaling:launchconfiguration"
-  #   name      = "SecurityGroups"
-  #   value     = module.security_groups.aws_security_group_ec2_to_bookingdbinstance_sg_id
-  # }
-
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
--- production/modules/security-groups/main.tf
+++ production/modules/security-groups/main.tf
@@ -17,15 +17,3 @@ resource "aws_security_group" "bookingdbinstance_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
-
-# resource "aws_security_group" "ec2_to_bookingdbinstance_sg" {
-#   name        = "ec2_to_bookingdbinstance_sg"
-#   description = "Allow outbound PostgreSQL traffic"
-
-#   egress {
-#     from_port   = 5432
-#     to_port     = 5432
-#     protocol    = "tcp"
-#     cidr_blocks = [aws_vpc.main.cidr_block]  # Adjust this based on your security requirements
-#   }
-# }
