e99fdb344f9701cc5dda1e72a457b535d39d1437
Alexandru Coarna <coarna.alexandru@gmail.com>
2023-11-22 14:22:55 +0200

    more testing

--- production/main.tf
+++ production/main.tf
@@ -168,13 +168,14 @@ resource "aws_route53_record" "main" {
   records = [aws_elastic_beanstalk_environment.env.cname]
 }
 
+data "aws_elastic_beanstalk_hosted_zone" "current" {}
 resource "aws_route53_record" "subdomain" {
   zone_id = module.route53.hosted_zone_id
   name    = ""
   type    = "A"
   alias {
     name                   = aws_elastic_beanstalk_environment.env.cname
-    zone_id                = module.route53.hosted_zone_id
+    zone_id                = data.aws_elastic_beanstalk_hosted_zone.current.id
     evaluate_target_health = true
   }
 }
