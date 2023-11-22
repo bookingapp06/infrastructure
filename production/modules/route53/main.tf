d4c3049721ba5f6c2a0f0a01729b6bb247b8ebc5
Alexandru Coarna <coarna.alexandru@gmail.com>
2023-11-22 15:05:50 +0200

    revert last changes

--- production/modules/route53/main.tf
+++ production/modules/route53/main.tf
@@ -10,7 +10,7 @@ data "aws_route53_zone" "booking_app_hosted_zone" {
 resource "aws_acm_certificate" "booking_app_hosted_certificate" {
   domain_name       = local.domain_name
   validation_method = "DNS"
-  subject_alternative_names = ["*.${local.domain_name}"]
+  # subject_alternative_names = ["*.${local.domain_name}"]
 
   lifecycle {
     create_before_destroy = true
