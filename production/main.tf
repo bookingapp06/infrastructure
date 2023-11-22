060cea62b8396c34b6fd03f24cb61c6aac6caeae
Alexandru Coarna <coarna.alexandru@gmail.com>
2023-11-22 12:58:36 +0200

    try point domain to ebs

--- production/main.tf
+++ production/main.tf
@@ -168,13 +168,13 @@ resource "aws_route53_record" "main" {
   records = [aws_elastic_beanstalk_environment.env.cname]
 }
 
-# resource "aws_route53_record" "main_domain" {
-#   zone_id = module.route53.hosted_zone_id
-#   name    = module.route53.domain_name
-#   type    = "A"
-#   alias {
-#     name                   = aws_elastic_beanstalk_environment.env.cname
-#     zone_id                = module.route53.hosted_zone_id
-#     evaluate_target_health = true
-#   }
-# }
+resource "aws_route53_record" "subdomain" {
+  zone_id = module.route53.hosted_zone_id
+  name    = ""
+  type    = "A"
+  alias {
+    name                   = aws_elastic_beanstalk_environment.env.cname
+    zone_id                = module.route53.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
